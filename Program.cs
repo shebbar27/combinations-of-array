3a69d8cb230f0f0b98b85dc715583084fcd0f6e9
Sunaada Hebbar <shebbarm@asu.edu>
2022-03-22 16:00:24 -0700

    working code

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+ 
+class Program
+{
+    static void getCombinationsOfGivenSize(IList<char> characters, IList<char> combinations, int start, int end, int currentIndex, int sizeOfCombinations, ref List<List<char>> result)
+    {
+        if (currentIndex == sizeOfCombinations)
+        {
+            List<char> temp = new List<char>();
+            for (int j = 0; j < sizeOfCombinations; j++)
+            {
+                temp.Add(combinations[j]);
+            }
+
+            result.Add(temp);
+            return;
+        }
+ 
+        for (int i = start; i <= end && end - i + 1 >= sizeOfCombinations - currentIndex; i++)
+        {
+            combinations[currentIndex] = characters[i];
+            getCombinationsOfGivenSize(characters, combinations, i + 1, end, currentIndex + 1, sizeOfCombinations, ref result);
+        }
+    }
+ 
+    static void printAllPossibleCombinations(IList<char> characters, int sizeOfCombinations, ref List<List<char>> result)
+    {
+        IList<char> combinations = Enumerable.Repeat(' ', sizeOfCombinations).ToList<char>();
+ 
+        getCombinationsOfGivenSize(characters, combinations, 0, characters.Count - 1, 0, sizeOfCombinations, ref result);
+    }
+    
+    static public void Main ()
+    {
+        IList<char> characters = new List<char>() {
+            'a',
+            'b',
+            'c',
+            'd',
+            //'e',
+        };
+
+        List<List<char>> result = new List<List<char>>();
+        for(int sizeOfCombinations=0; sizeOfCombinations <= characters.Count; sizeOfCombinations++) 
+        {
+            printAllPossibleCombinations(characters, sizeOfCombinations, ref result);
+        }
+
+        foreach(var list in result) 
+        {
+            foreach(var ch in list) 
+            {
+                Console.Write(ch + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+#Combinations of Array
+
+This program gets all possible combinations that can be formed from a list of characters without altering the relative positions of the array elements.
+
+Examlple Test Cases:
+
+1.  Input: {a, b, c}
+    Output: {{}, {a}, {b}, {c}, {a, b}, {a, c}, {b, c}, {a, b, c}}
+
+2.  Input: { a, b, c, d }
+    Output: {{}, {a}, {b}, {c}, {d}, {a, b}, {a, c}, {a, d}, {b, c}, {b, d}, {c, d}, {a, b, c}, {a, b, d}, {a, c, d}, {b, c, d}, {a, b, c, d}}
